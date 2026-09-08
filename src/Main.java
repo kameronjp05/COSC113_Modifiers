32bc0e5baad4b38685a8235533a04517d946c21b
Kameron Peart <peartk1027@students.bowiestate.edu>
2026-09-08 09:53:19 -0400

    Modifiers- 2nd commit - encapsulation

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+//public means accessible from anywhere
+//private means accessible from inside the class
+
+    public static void main(String[] args) {
+
+        Student s8, s9, s10;
+        s8 = new Student();
+        //s8.gpa = 4.0;
+        s8.Fn = "Praise";
+        s8.Ln = "Ben";
+        //s8.gpa=-3.1;
+        s8.setGpa(3.0);
+
+        //code from sept 3 class
+        Student s6 = new Student();
+        s6.Fn = "Melanie";
+        s6.setLn("Thomas");
+        s6.Ln = "Simpson";
+
+        Student s4 = new Student("Helena", "Payton" );
+        System.out.println(s4.Fn);
+        System.out.println(s4.Ln);
+        System.out.println(s4.Sid);
+
+
+        Student s1;
+        s1 = new Student();
+        System.out.println(s1);
+        System.out.println(s1.Fn);
+
+        Student s2;
+        s2 = new Student("tobiloba", "Ayodeji", 14141);
+        System.out.println(s2.Fn);
+        System.out.println(s2.Ln);
+
+        Student s3;
+        String Fn = "Miguel";
+        String Ln = "Gascaortega";
+        int Sid = 31415;
+        s3 = new Student(Fn, Ln, Sid);
+        System.out.println(s3.Fn);
+        System.out.println(s3.Ln);
+
+        Student s5;
+        s5 = new Student(Fn, Sid, Ln);
+
+
+
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,23 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Student {
 
-    String Fn;
+    public String Fn;
 
-    String Ln;
+    public String Ln;
 
-    int Sid;
+    public int Sid;
+
+    private double gpa;
+
+//Encapsulation is a concept
 
     Student(){
         this.Fn= "no first name";
         this.Ln= "no last name";
         this.Sid= 0;
+        this.gpa=0.0;
     }
 
-    Student(String Fn, String Ln)  {
-        this.Fn = Fn;
-        this.Ln = Ln;
-    }
+//    Student(String Fn, String Ln)  {
+//        this.Fn = Fn;
+//        this.Ln = Ln;
+//    }
 
     Student(String Fn, String Ln, int Sid) {
         this(Fn,Ln);
@@ -55,41 +58,16 @@ public class Student {
         Ln = ln;
     }
 
-    public static void main(String[] args) {
+    public double getGpa() {
+        return gpa;
+    }
 
-        Student s6 = new Student();
-        s6.Fn = "Melanie";
-        s6.setLn("Thomas");
-        s6.Ln = "Simpson";
-
-        Student s4 = new Student("Helena", "Payton" );
-        System.out.println(s4.Fn);
-        System.out.println(s4.Ln);
-        System.out.println(s4.Sid);
-
-
-        Student s1;
-        s1 = new Student();
-        System.out.println(s1);
-        System.out.println(s1.Fn);
-
-        Student s2;
-        s2 = new Student("tobiloba", "Ayodeji", 14141);
-        System.out.println(s2.Fn);
-        System.out.println(s2.Ln);
-
-        Student s3;
-        String Fn = "Miguel";
-        String Ln = "Gascaortega";
-        int Sid = 31415;
-        s3 = new Student(Fn, Ln, Sid);
-        System.out.println(s3.Fn);
-        System.out.println(s3.Ln);
-
-        Student s5;
-        s5 = new Student(Fn, Sid, Ln);
-
-
-
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+        if (gpa >= 0.0 && gpa <= 4.0) {
+            this.gpa = gpa;
+        } else {
+            System.out.println("Invalid GPA value");
+        }
     }
 }
